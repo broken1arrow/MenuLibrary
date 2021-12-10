88c5843a9d9ec70ab646bc1cef1f7dc831bb39d4
broken1arrow <broken2arrow1@gmail.com>
2021-12-10 09:15:01 +0100

    Update MenuHolderListener.java
    added so you can´t drag item to a slot and make it so it will only deny on custom inventory.
    
    fixed so you can´t shift in items if menu not allow it.

--- src/main/java/brokenarrow/menu/lib/MenuHolderListener.java
+++ src/main/java/brokenarrow/menu/lib/MenuHolderListener.java
@@ -7,10 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
@@ -18,77 +15,64 @@ import java.util.ListIterator;
 
 public class MenuHolderListener implements Listener {
 
-
 	private final MenuCache menuCache = MenuCache.getInstance();
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void onMenuClicking(InventoryClickEvent event) {
 		Player player = (Player) event.getWhoClicked();
 
-		if (event.getClickedInventory() != null && event.getClickedInventory().getType() == InventoryType.PLAYER)
+		if (event.getClickedInventory() == null)
 			return;
+		ItemStack clickedItem = event.getCurrentItem();
+		ItemStack cursor = event.getCursor();
 
 		MenuHolder menu = getMenuHolder(player);
-
 		if (menu == null) return;
-		System.out.println("menu are not null " + menu.getMenu() + " " + event.getView().getTopInventory());
 		if (!event.getView().getTopInventory().equals(menu.getMenu())) return;
 
+		if (!menu.isSlotsYouCanAddItems() && cursor != null && cursor.getType() != Material.AIR) {
+			event.setCancelled(true);
+		}
+
 		if (!menu.getButtons().isEmpty()) {
-			ItemStack clickedItem = event.getCurrentItem();
 			int clickedSlot = event.getSlot();
 			int clickedPos = menu.getPageNumber() * menu.getMenu().getSize() + clickedSlot;
-			if (clickedItem == null || clickedItem.getType() == Material.AIR) return;
+
 			if (menu.isAllowShiftClick() && (event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT))
 				return;
-
 			if (menu.isSlotsYouCanAddItems()) {
 				if (menu.getFillSpace().contains(clickedPos))
 					return;
 				else
 					event.setCancelled(true);
+			} else if (!menu.isSlotsYouCanAddItems()) {
+				if (event.getClickedInventory().getType() == InventoryType.PLAYER)
+					if (event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT) {
+						event.setCancelled(true);
+					} else
+						event.setCancelled(true);
 			}
 
+			if (clickedItem != null)
+				for (ListIterator<MenuButton> menuButtons = menu.getButtons().listIterator(); menuButtons.hasNext(); ) {
+					MenuButton menuButton = menuButtons.next();
+					Object objectData = menu.getObject() != null && !menu.getObject().equals("") ? menu.getObject() : clickedItem;
 
-			for (ListIterator<MenuButton> menuButtons = menu.getButtons().listIterator(); menuButtons.hasNext(); ) {
-				MenuButton menuButton = menuButtons.next();
-				Object objectData = menu.getObject() != null && !menu.getObject().equals("") ? menu.getObject() : clickedItem;
-
-				if (menuButton.getItem() != null && menuButton.getItem().isSimilar(clickedItem)) {
-					if (menu.getAddedButtons().containsKey(menu.getPageNumber()) && menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos) != null && menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos).isSimilar(clickedItem)) {
+					if (menuButton.getItem() != null && menuButton.getItem().isSimilar(clickedItem)) {
+						if (menu.getAddedButtons().containsKey(menu.getPageNumber()) && menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos) != null && menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos).isSimilar(clickedItem)) {
+							menuButton.onClickInsideMenu(player, menu.getMenu(), event.getClick(), clickedItem, objectData);
+							event.setCancelled(true);
+							break;
+						}
+					} else if (menuButton.getItem(objectData) != null && isItemSimilar(menuButton.getItem(objectData), clickedItem) && menu.getAddedButtons().containsKey(menu.getPageNumber()) && isItemSimilar(menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos), clickedItem)) {
 						menuButton.onClickInsideMenu(player, menu.getMenu(), event.getClick(), clickedItem, objectData);
 						event.setCancelled(true);
 						break;
 					}
-				} else if (menuButton.getItem(objectData) != null && isItemSimilar(menuButton.getItem(objectData), clickedItem) && menu.getAddedButtons().containsKey(menu.getPageNumber()) && isItemSimilar(menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos), clickedItem)) {
-					menuButton.onClickInsideMenu(player, menu.getMenu(), event.getClick(), clickedItem, objectData);
-					event.setCancelled(true);
-					break;
 				}
-			}
-
-		}
-		if (!menu.isSlotsYouCanAddItems()) {
-			event.setCancelled(true);
 		}
 	}
 
-
-	public boolean isItemSimilar(ItemStack item, ItemStack clickedItem) {
-		if (item != null && clickedItem != null)
-			if (clickedItem.getType() == Material.PLAYER_HEAD && clickedItem.getType() == item.getType()) {
-				SkullMeta skullMetaClicked = (SkullMeta) clickedItem.getItemMeta();
-				SkullMeta skullMetaItem = (SkullMeta) item.getItemMeta();
-				if (item.getItemMeta().getDisplayName().equals(clickedItem.getItemMeta().getDisplayName()))
-					return skullMetaClicked.getOwningPlayer() == skullMetaItem.getOwningPlayer();
-				else if (item.getItemMeta().getLore() == clickedItem.getItemMeta().getLore())
-					return skullMetaClicked.getOwningPlayer() == skullMetaItem.getOwningPlayer();
-				else return skullMetaClicked.getOwningPlayer() == skullMetaItem.getOwningPlayer();
-
-			} else item.isSimilar(clickedItem);
-
-		return false;
-	}
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void onMenuClose(InventoryCloseEvent event) {
@@ -100,19 +84,82 @@ public class MenuHolderListener implements Listener {
 		if (!event.getView().getTopInventory().equals(menu.getMenu()))
 			return;
 
-
-		System.out.println("inventory close ");
-
 		menu.onMenuClose(event);
 		menu.menuClose(event, menu.getMenu());
 
 	}
 
+	@EventHandler(priority = EventPriority.LOW)
+	public void onInventoryDragTop(final InventoryDragEvent event) {
+		final Player player = (Player) event.getWhoClicked();
+		if (event.getView().getType() == InventoryType.PLAYER) return;
+
+		MenuHolder menu = getMenuHolder(player);
+		if (menu == null) return;
+
+		if (!menu.getButtons().isEmpty()) {
+			final int size = event.getView().getTopInventory().getSize();
+
+			for (int clickedSlot : event.getRawSlots()) {
+				if (clickedSlot > size)
+					continue;
+
+				int clickedPos = menu.getPageNumber() * menu.getMenu().getSize() + clickedSlot;
+
+				final ItemStack cursor = checkIfNull(event.getCursor(), event.getOldCursor());
+				if (menu.isSlotsYouCanAddItems()) {
+					if (menu.getFillSpace().contains(clickedPos))
+						return;
+					else
+						event.setCancelled(true);
+				} else if (!menu.isSlotsYouCanAddItems()) {
+					event.setCancelled(true);
+				}
+
+				if (cursor != null)
+					for (ListIterator<MenuButton> menuButtons = menu.getButtons().listIterator(); menuButtons.hasNext(); ) {
+						MenuButton menuButton = menuButtons.next();
+						Object objectData = menu.getObject() != null && !menu.getObject().equals("") ? menu.getObject() : cursor;
+
+						if (menuButton.getItem() == null) {
+							if (!menu.getAddedButtons().containsKey(menu.getPageNumber()) && menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos) == null && !menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos).isSimilar(cursor)) {
+								event.setCancelled(true);
+								break;
+							}
+						} else if (menuButton.getItem(objectData) == null && !isItemSimilar(menuButton.getItem(objectData), cursor) && !menu.getAddedButtons().containsKey(menu.getPageNumber()) && !isItemSimilar(menu.getAddedButtons().get(menu.getPageNumber()).get(clickedPos), cursor)) {
+							event.setCancelled(true);
+							break;
+						}
+					}
+			}
+		}
+	}
+
+	public boolean isItemSimilar(ItemStack item, ItemStack clickedItem) {
+		if (item != null && clickedItem != null)
+			if (clickedItem.getType() == Material.PLAYER_HEAD && clickedItem.getType() == item.getType()) {
+				SkullMeta skullMetaClicked = (SkullMeta) clickedItem.getItemMeta();
+				SkullMeta skullMetaItem = (SkullMeta) item.getItemMeta();
+
+				if (item.getItemMeta().getDisplayName().equals(clickedItem.getItemMeta().getDisplayName()))
+					return skullMetaClicked.getOwningPlayer() == skullMetaItem.getOwningPlayer();
+				else if (item.getItemMeta().getLore() == clickedItem.getItemMeta().getLore())
+					return skullMetaClicked.getOwningPlayer() == skullMetaItem.getOwningPlayer();
+				else return skullMetaClicked.getOwningPlayer() == skullMetaItem.getOwningPlayer();
+
+			} else item.isSimilar(clickedItem);
+
+		return false;
+	}
+
+	public ItemStack checkIfNull(ItemStack curentCursor, ItemStack oldCursor) {
+		return curentCursor != null ? curentCursor : oldCursor != null ? oldCursor : new ItemStack(Material.AIR);
+	}
 
 	private MenuHolder getMenuHolder(Player player) {
 
 		Location location = null;
-		Object object = null;
+		Object object;
 		if (player.hasMetadata(MenuMetadataKey.MENU_OPEN_LOCATION.name())) {
 			object = player.getMetadata(MenuMetadataKey.MENU_OPEN_LOCATION.name()).get(0).value();
 			if (object instanceof Location)
@@ -127,5 +174,6 @@ public class MenuHolderListener implements Listener {
 		}
 		return menu;
 	}
+
 
 }
