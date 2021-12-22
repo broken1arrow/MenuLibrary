b10efdd165bb71d8907507f499d01cf800990504
broken1arrow <broken2arrow1@gmail.com>
2021-12-22 15:50:38 +0100

    Update SendMsgDuplicatedItems.java
    change name from SendMsg to SendMsgDublicaded .

--- src/main/java/brokenarrow/menu/lib/messages/SendMsgDuplicatedItems.java
+++ src/main/java/brokenarrow/menu/lib/messages/SendMsgDuplicatedItems.java
@@ -1,15 +1,14 @@
 package brokenarrow.menu.lib.messages;
 
 
-import org.bukkit.ChatColor;
+import org.broken.lib.rbg.TextTranslator;
 import org.bukkit.entity.Player;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class SendMsg {
-
-	private static final Pattern HEX_PATTERN = Pattern.compile("(?<!\\\\\\\\)(#[a-fA-F0-9]{6})");
+/**
+ * Set messages when player add duplicated items or items you have blacklisted.
+ *
+ */
+public class SendMsgDuplicatedItems {
 	private static String blacklistMessage;
 	private static String dublicatedMessage;
 
@@ -17,21 +16,21 @@ public class SendMsg {
 	 * Set message for when player have added item some are blacklisted.
 	 * Suport both hex and & colorcodes and have this placeholders:
 	 *
-	 * hex format is {#8000ff}
+	 * hex format is <#8000ff> and gradients <#8000ff:#8000ff>
 	 * {0} = item type
 	 *
 	 * @param blacklistMessage set a message.
 	 */
 
 	public static void setBlacklistMessage(String blacklistMessage) {
-		SendMsg.blacklistMessage = blacklistMessage;
+		SendMsgDuplicatedItems.blacklistMessage = blacklistMessage;
 	}
 
 	/**
 	 * Set message for when player have added item some are dublicated.
 	 * Suport both hex and & colorcodes and have this placeholders:
 	 *
-	 * hex format is {#8000ff}
+	 * hex format is <#8000ff> and gradients <#8000ff:#8000ff>
 	 * {0} = item type
 	 * {1} = amount of stacks
 	 * {2} = item amount
@@ -40,7 +39,7 @@ public class SendMsg {
 	 */
 
 	public static void setDublicatedMessage(String dublicatedMessage) {
-		SendMsg.dublicatedMessage = dublicatedMessage;
+		SendMsgDuplicatedItems.dublicatedMessage = dublicatedMessage;
 	}
 
 	public static void sendMessage(Player player, String msg) {
@@ -54,7 +53,7 @@ public class SendMsg {
 		else
 			message = blacklistMessage;
 
-		player.sendMessage(translateHexCodes( translatePlaceholders(message, placeholders)));
+		player.sendMessage(TextTranslator.toSpigotFormat(translatePlaceholders(message, placeholders)));
 	}
 
 	public static void sendDublicatedMessage(Player player, Object... placeholders) {
@@ -64,7 +63,7 @@ public class SendMsg {
 		else
 			message = dublicatedMessage;
 
-		player.sendMessage(translateHexCodes(translatePlaceholders(message, placeholders)));
+		player.sendMessage(TextTranslator.toSpigotFormat(translatePlaceholders(message, placeholders)));
 	}
 
 	public static String translatePlaceholders(String rawText, Object... placeholders) {
@@ -72,21 +71,5 @@ public class SendMsg {
 			rawText = rawText.replace("{" + i + "}", placeholders[i] != null ? placeholders[i].toString() : "");
 		}
 		return rawText;
-	}
-
-	public static String translateHexCodes(String textToTranslate) {
-		String[] hexSplitedToFitRBG = new String[0];
-		Matcher matcher = HEX_PATTERN.matcher(textToTranslate);
-		StringBuilder buffer = new StringBuilder();
-
-		if (matcher.find()) {
-			hexSplitedToFitRBG = matcher.group(1).split("");
-			if (!(hexSplitedToFitRBG.length == 7))
-				System.out.println("you has wrongly set up the hex color it shall be 6 in length");
-		}
-		if (hexSplitedToFitRBG.length == 7)
-			return ChatColor.translateAlternateColorCodes('&', "&x&" + hexSplitedToFitRBG[1] + "&" + hexSplitedToFitRBG[2] + "&" + hexSplitedToFitRBG[3] + "&" + hexSplitedToFitRBG[4] + "&" + hexSplitedToFitRBG[5] + "&" + hexSplitedToFitRBG[6] + textToTranslate.replace("<" + matcher.group(1) + ">", ""));
-
-		return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString().replace("<", "").replace(">", ""));
 	}
 }
