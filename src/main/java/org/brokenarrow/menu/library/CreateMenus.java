b159c48d60f73d24df791dcfb7ac8271746f75b0
broken1arrow <broken2arrow1@gmail.com>
2022-06-10 16:22:14 +0200

    fixed 1 bug

--- src/main/java/org/brokenarrow/menu/library/CreateMenus.java
+++ src/main/java/org/brokenarrow/menu/library/CreateMenus.java
@@ -933,6 +933,8 @@ public class CreateMenus {
 				result = menuButton.getItem(objectFromlistOfFillItems);
 				if (result == null)
 					result = menuButton.getItem(oldSlotIndex, objectFromlistOfFillItems);
+				if (result == null)
+					result = menuButton.getItem();
 			}
 		} else {
 			result = menuButton.getItem();
